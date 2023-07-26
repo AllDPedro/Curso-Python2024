7b2308c0b753fc3483f5a90be09bc45531d5a646
Allan Delon Pedro <86141527+AllDPedro@users.noreply.github.com>
2023-07-26 09:08:04 -0300

    Add files via upload

---
+++ A_3_Logica_de_programacao_basica/Aula3.py
@@ -0,0 +1,23 @@
+''' 
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica /  Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+'''
+
+# ASPAS simples ' ' 
+print('Estou usando ASPAS Simples para mostrar essa mensagem = ' ' ')
+
+# ASPAS duplas " "
+print("Estou usando ASPAS Duplas para mostrar essa mensagem = " " ")
+
+# Escape 
+print("Allan Delon Pedro \" da Silva Farias") # o Escape é o \ para não interpretar a aspas duplas que está do lado dele e sim mostrar ela.
+
+# r 
+print(r"Allan Delon Pedro \" da Silva Farias\"" ) # Colocando o r no começo antes das aspas duplas mostra tudo no código.
+
+## Tem como colocar aspas duplas dentro das aspas simples e vice-versa. 
+
+
---
+++ A_3_Logica_de_programacao_basica/aula1.py
@@ -0,0 +1,11 @@
+"""
+DocString é para comentar múltiplas Linhas.
+Serve para escrever suas notas.
+ Áspas  triplas para comentários 
+"""
+''' Pode usar para escrever notas '''
+
+# Permite escrever um comentário = Cerquilha
+print(123) # Na frente
+# Abaixo 
+print(456)
